a7784e4581e121de8cc5eebad392ef18dbba23e0
linnall <linna.luo@uniswap.org>
2023-08-15 18:10:54 -0400

    changes slash command name

--- server.py
+++ server.py
@@ -17,12 +17,13 @@ ssl._create_default_https_context = ssl._create_unverified_context
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
-@app.route("/slack/test", methods=["POST"])
+@app.route("/slack/add-address", methods=["POST"])
 def command():
   if not verifier.is_valid_request(request.get_data(), request.headers):
     return make_response("invalid request", 403)
   info = request.form
 
+  # receives modal submission input
   if "payload" in info:
     payload = json.loads(request.form["payload"])
     if payload["type"] == "view_submission":
@@ -94,29 +95,6 @@ def command():
     logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
     logging.error(e.response)
     return make_response("", e.response.status_code)
-
-  return make_response("", response.status_code)
-
-@app.route("/slack/submit", methods=["POST"])
-def submit():
-  print("we get here")
-  if not verifier.is_valid_request(request.get_data(), request.headers):
-    return make_response("invalid request", 403)
-  info = request.form
-  print("REQUEST FORM", "payload" in info)
-
-  # # send user a response via DM
-  # im_id = slack_client.im_open(user=info["user_id"])["channel"]["id"]
-  # ownerMsg = slack_client.chat_postMessage(
-  #   channel=im_id,
-  #   text="Thanks for submitting a collection!"
-  # )
-
-  # send channel a response
-  response = slack_client.chat_postMessage(
-    channel='#{}'.format(info["channel_name"]), 
-    text="Thanks for submitting a collection!"
-  )
 
   return make_response("", response.status_code)
 
